d8e63c42bf7459fad90daf35d939226522c15ec9
Sufia Parveen <sufia.parveen@mail.waldenu.edu>
2023-07-04 01:54:16 +0530

    xcv

--- build/setup-org.sh
+++ build/setup-org.sh
@@ -1,9 +1,9 @@
 echo "Setting up Org Connection..."
 mkdir keys
-echo $SFDC_SERVER_KEY
 
+echo $SFDC_CLIENT_ID
 echo $SFDC_SERVER_KEY > keys/server.key
 
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_CLIENT_ID --jwtkeyfile keys/server.key --username $SFDC_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --instanceurl https://login.salesforce.com --clientid $SFDC_CLIENT_ID --jwtkeyfile keys/server.key --username $SFDC_USERNAME --setdefaultdevhubusername -a DevHub
 sfdx force:org:list
