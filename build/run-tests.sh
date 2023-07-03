3df49523cabfa60fe7c78e31163b8d2c879e9b53
Sufia Parveen <sufia.parveen@mail.waldenu.edu>
2023-07-04 02:29:19 +0530

    setup

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -1,1 +1,1 @@
-sf apex run test --api-version ${VAR_API_VERSION} --target-org prod-org --test-level RunLocalTests --code-coverage --detailed-coverage --result-format junit --wait 30 > tests.xml
+sf apex run test --api-version 58.0 --target-org prod-org --test-level RunLocalTests --code-coverage --detailed-coverage --result-format junit --wait 30 > tests.xml
