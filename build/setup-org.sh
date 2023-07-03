b72bfa3ea6a201b484ee0602af2ed2885c8c1bcd
Sufia Parveen <sufia.parveen@mail.waldenu.edu>
2023-07-04 01:34:28 +0530

    xcx

--- build/setup-org.sh
+++ build/setup-org.sh
@@ -1,6 +1,8 @@
 echo "Setting up Org Connection..."
 mkdir keys
-echo $SFDC_SERVER_KEY | > keys/server.key
+echo $SFDC_SERVER_KEY
+
+echo $SFDC_SERVER_KEY > keys/server.key
 
 echo "Authenticating..."
 sfdx force:auth:jwt:grant --clientid $SFDC_CLIENT_ID --jwtkeyfile keys/server.key --username $SFDC_USERNAME --setdefaultdevhubusername -a DevHub
