9e6a3024cdcd437968aea82cc3cf9af4bfb9aaf5
Sufia Parveen <sufia.parveen@mail.waldenu.edu>
2023-07-04 01:25:26 +0530

    push

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,8 @@
 echo "Installing Dependencies... "
 sudo npm install sfdx-cli --global
 sudo npm install @salesforce/cli --global
+
+echo "Installing plugins... "
+echo y |sfdx plugins:install https://github.com/Accenture/sfpowerkit -f
+echo "Installing jq to read json... "
+sudo apt-get install jq
--- build/setup-org.sh
+++ build/setup-org.sh
@@ -1,6 +1,6 @@
 echo "Setting up DevHub Connection..."
 mkdir keys
-echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
+echo $SFDC_SERVER_KEY | > keys/server.key
 
 echo "Authenticating..."
 RES=$(sfdx force:auth:jwt:grant --clientid $SFDC_CLIENT_ID --jwtkeyfile keys/server.key --username $SFDC_USERNAME --setdefaultdevhubusername -a DevHub --json)
